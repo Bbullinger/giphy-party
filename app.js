22b6ba2c5ebc418156c8a65f99666f62e9dbcdda
Brandon <brandon.bullinger@yahoo.com>
2022-09-21 23:47:07 -0400

    search/posting of gif functional

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
+const gifContainer = document.querySelector("#gifContainer");
+const gifSearch = document.querySelector("#gifSearch");
+const userInput = document.querySelector("#userInput");
+const removeGIFs = document.querySelector("#removeGIFs");
+
 function createHTML(el) {
   return document.createElement(el);
 }
-async function userSearch(search.value) {
-  console.log("test");
-  const response = await axios.get(
-    `http://api.giphy.com/v1/gifs/search?api_key=RqFMYNpWWi9Hx6MCuHcbN5XtL51yVjuR&q=${search.value}&limit=5`
-  );
-  console.log(response);
+
+//This function will search the giphy api for a gif, based off the param (user input from search form)
+async function userSearch(search) {
+  //MY GIPHY TOKEN: RqFMYNpWWi9Hx6MCuHcbN5XtL51yVjuR
+  if (search) {
+    const response = await axios.get(
+      `http://api.giphy.com/v1/gifs/search?api_key=RqFMYNpWWi9Hx6MCuHcbN5XtL51yVjuR&q=${search}&limit=1`
+    );
+
+    let newImg = createHTML("img");
+
+    newImg.setAttribute("id", response.data.data[0].images.original.url);
+    newImg.src = response.data.data[0].images.original.url;
+    gifContainer.append(newImg);
+  }
 }
 
-//MY GIPHY TOKEN: RqFMYNpWWi9Hx6MCuHcbN5XtL51yVjuR
+gifSearch.addEventListener("submit", function (e) {
+  e.preventDefault();
+  userSearch(userInput.value);
+  userInput.value = "";
+});
